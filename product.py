f2430302f18e6343b2ef5d4e8d28507d249f514d
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-07-22 18:50:43 +0200

    Fix raw_product creation when raw product has no code

--- product.py
+++ product.py
@@ -297,9 +297,11 @@ class Product:
 
         logging.getLogger(self.__name__).info("create_main_product(%s)" % self)
         with Transaction().set_context(no_create_raw_products=True):
+            code = self.code
+            if config and config.main_product_prefix and code:
+                code = code.replace(config.main_product_prefix, '')
             raw_product, = self.copy([self], default={
-                    'code': self.code.replace(config.main_product_prefix, '')
-                        if config.main_product_prefix else self.code,
+                    'code': code,
                     'is_raw_product': True,
                     'main_product': self.id,
                     })
